128f54f802775d7d9336ff7301538ada7bbe393e
github username <github email address>
2019-01-24 15:21:02 -0500

    outline of front end 1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  yesNoButtonDiv = document.querySelector(".ui.large.buttons")
+
+  yesNoButtonDiv.addEventListener("click", (e)=>{
+    console.log("yeet")
+    if(e.target.className === "button"){  
+    }
+  })
+
+})
