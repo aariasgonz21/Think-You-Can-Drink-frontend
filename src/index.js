c08bfb1ed431b179c7cc4111c6b66eb1cad6884a
github username <github email address>
2019-01-25 15:07:52 -0500

    fetch create user post is functional

--- src/index.js
+++ src/index.js
@@ -4,31 +4,59 @@ document.addEventListener("DOMContentLoaded", () => {
   let yesNoButtonDiv = document.querySelector(".ui.large.buttons")
   let weightDiv = document.querySelector('#ui-input')
   let typeDrinkDiv = document.querySelector('#drink-type')
+  let wineDiv = document.querySelector('#wine-div');
+  let beerDiv = document.querySelector('#beer-div');
+  let mixedDiv = document.querySelector('#mixed-drinks-div');
+  let shotsDiv = document.querySelector('#shots-div');
+  let contentColumn = document.querySelector('#content-column')
+  let submitButton = document.querySelector('#info-submit-1')
 
-  // console.log(typeDrinkDiv)
+  submitButton.addEventListener("click", (e) => { 
+    fetch('http://localhost:3000/api/v1/user_lists', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify({
+        sex: userInfo.sex,
+        weight: userInfo.weight
+      })
+    }).then((res) => {
+      return res.json()
+    }).then((data) => {
+      console.log(data)
+    })
+  })
+  
+  contentColumn.addEventListener("click", (e)=>{
+    //debugger
+    if (e.target.nodeName.toLowerCase() === 'button'){
+      let sex = e.target.innerText.toLowerCase();
+      userInfo['sex'] = sex
+    }
+    
+    
 
-  yesNoButtonDiv.addEventListener("click", (e)=>{
-      if (e.target.className === 'ui button') {
-        let selectedFMButton = e.target.innerText;
-        userInfo['sex'] = selectedFMButton.toLowerCase();
-      }
   })
 
   weightDiv.addEventListener('change', inputWeightHandler)
   typeDrinkDiv.addEventListener('click', handleTypesOfDrinks);
+  wineDiv.addEventListener('click', handleWineDivClick); 
+  mixedDiv.addEventListener('click', handleMixedDivClick); 
+  shotsDiv.addEventListener('click', handleShotsDivClick); 
+  beerDiv.addEventListener('click', handleBeerDivClick); 
 })
 
 function inputWeightHandler (event) {
-  console.log(event.target.value)
    let userWeightVal = event.target.value;
    userInfo['weight'] = userWeightVal
 }
 
 function handleTypesOfDrinks (event) {
-  //click - wine
   if (event.target.id === "btn-wine") {
     let wineDivArea = document.querySelector('#wine-div');
-
+  
     if (wineDivArea.style.display === "none") {
       wineDivArea.style.display = "block";
     } else {
@@ -120,7 +148,6 @@ function handleTypesOfDrinks (event) {
     //append the ul to the div
     shotsDivArea.append(createShotsUl)
     let selectedValShots = event.target.innerText.toLowerCase();
-
     fetch('http://localhost:3000/api/v1/alcohols')
       .then((res) => {
         return res.json()
@@ -152,16 +179,17 @@ function filterDrinksForValOnly(arr, val) {
 
 function renderWineFilteredList(obj) {
   let wineUl = document.querySelector('.wine-ul');
+  let parentDivOfLi = document.createElement('div');
+
   //create li
   let wineLi = document.createElement('li');
   wineLi.style = "list-style-type: none"
   wineLi.dataset.id = obj.id
   wineLi.id = `wine-${obj.id}`
-  wineUl.innerHTML += dropdown()
+  wineLi.innerHTML = `<p> ${obj.name}</p>`
+  wineLi.innerHTML += dropdown(obj)
   $('.ui.dropdown')
-  .dropdown()
-  ;
-  wineLi.innerText = obj.name
+    .dropdown();
   //append li to the ul
   wineUl.appendChild(wineLi)
 }
@@ -172,12 +200,11 @@ function renderBeerFilteredList(obj) {
   beerLi.style = "list-style-type: none"
   beerLi.dataset.id = obj.id
   beerLi.id = `beer-${obj.id}`
-  beerLi.innerText = obj.name
-  beerUl.innerHTML += dropdown()
+  beerLi.innerHTML = `${obj.name} <br/>`
+  beerLi.innerHTML += dropdown(obj)
   $('.ui.dropdown')
-  .dropdown()
-  ;
-  //append li to the ul
+  .dropdown();
+
   beerUl.appendChild(beerLi)
 }
 function renderMixedFilteredList(obj) {
@@ -187,12 +214,11 @@ function renderMixedFilteredList(obj) {
   mixedLi.style = "list-style-type: none"
   mixedLi.dataset.id = obj.id
   mixedLi.id = `mixed-${obj.id}`
-  mixedLi.innerText = obj.name
-  mixedUl.innerHTML += dropdown()
+  mixedLi.innerHTML = `${obj.name} <br/>`
+  mixedLi.innerHTML += dropdown(obj)
   $('.ui.dropdown')
-  .dropdown()
-  ;
-  //append li to the ul
+  .dropdown();
+ 
   mixedUl.appendChild(mixedLi)
 }
 
@@ -203,22 +229,20 @@ function renderShotsFilteredList(obj) {
   shotsLi.style = "list-style-type: none"
    shotsLi.dataset.id = obj.id
    shotsLi.id = `shots-${obj.id}`
-   shotsLi.innerText = obj.name
-   shotsUl.innerHTML += dropdown()
+   shotsLi.innerHTML = `${obj.name} <br/>`
+   shotsLi.innerHTML += dropdown(obj)
    $('.ui.dropdown')
-   .dropdown()
-   ;
-  //append li to the ul
+   .dropdown();
+
  shotsUl.appendChild(shotsLi);
 }
 
-function dropdown() {
-  return `<div class="ui selection dropdown">
-  <input type="hidden" name="gender">
-  <i class="dropdown icon"></i>
+function dropdown(obj) {
+  return `<div id="${obj.name}" class="ui selection dropdown">
+  <input type="hidden" name="drink">
+  <i class="dropdown icon" id="${obj.name}" data-id="${obj.id}"></i>
   <div class="default text">How Many Drinks</div>
     <div class="menu">
-      <div class="item" data-value="0">0</div>
       <div class="item" data-value="1">1</div>
       <div class="item" data-value="2">2</div>
       <div class="item" data-value="3">3</div>
@@ -228,7 +252,55 @@ function dropdown() {
       <div class="item" data-value="7">7</div>
       <div class="item" data-value="8">8</div>
       <div class="item" data-value="9">9</div>
-      <div class="item" data-value="10">10???</div>
+      <div class="item" data-value="10">10</div>
     </div>
   </div>`
+}
+
+userInfo['drinks']= [];
+function handleWineDivClick (event) {
+  let newHash = {};
+  if (event.target.classList.contains('item')) {
+  newHash[event.target.parentElement.parentElement.id] = event.target.getAttribute("data-value")
+  userInfo["drinks"].push(newHash);
+  }
+}
+
+function handleBeerDivClick(event) {
+  let newHash = {};
+  if (event.target.classList.contains('item')) {
+    newHash[event.target.parentElement.parentElement.id] = event.target.getAttribute("data-value")
+    userInfo["drinks"].push(newHash);
+  }
+}
+function handleMixedDivClick(event) {
+  let newHash = {};
+  if (event.target.classList.contains('item')) {
+    newHash[event.target.parentElement.parentElement.id] = event.target.getAttribute("data-value")
+    userInfo["drinks"].push(newHash);
+  }
+}
+
+function handleShotsDivClick(event) {
+  let newHash = {};
+  if (event.target.classList.contains('item')) {
+    newHash[event.target.parentElement.parentElement.id] = event.target.getAttribute("data-value")
+    userInfo["drinks"].push(newHash);
+  }
+}
+
+function handleUserListInfo (userInfo) {
+fetch('http://localhost:3000/api/v1/user_lists', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accepts': 'application/json'
+  },
+  body: JSON.stringify({sex: userInfo.sex,
+        weight: userInfo.weight})
+}).then((res)=>{
+  return res.json()
+}).then((data)=>{
+  console.log(data)
+})
 }
